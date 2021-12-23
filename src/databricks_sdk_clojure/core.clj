4faec17e6c2d31d9aab227ab8fe66ebb56faed78
Marina Cavalari <marina.cavalari.mc@gmail.com>
2021-12-22 22:49:20 -0300

    first commit

---
+++ src/databricks_sdk_clojure/core.clj
@@ -0,0 +1,6 @@
+(ns databricks-sdk-clojure.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/databricks_sdk_clojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns databricks-sdk-clojure.core-test
+  (:require [clojure.test :refer :all]
+            [databricks-sdk-clojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
