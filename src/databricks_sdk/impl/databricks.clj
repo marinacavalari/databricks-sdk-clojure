a8dbdd29e6d6d0d8e48b94aa3a4df980fb11418c
Marina Cavalari <marina.cavalari.mc@gmail.com>
2021-12-27 19:40:30 -0300

    refactor

--- src/databricks_sdk/impl/databricks.clj
+++ src/databricks_sdk/impl/databricks.clj
@@ -1,24 +1,39 @@
 (ns databricks-sdk.impl.databricks
   (:require [databricks-sdk.impl.endpoints :as endpoints]
-            [selmer.parser :as parser]
-            [org.httpkit.sni-client :as sni-client]))
+            [org.httpkit.client :as http]
+            [clojure.data.json :as json]))
 
-(defn base-request [host api-token timeout]
-  {:host host
-   :timeout timeout
-   :headers {"Authorization" (str "Bearer " api-token)}})
+(defn request [token url]
+  (let [token  "bla"
+        url "http://bla.databrics.cloud/api/2.0/clusters/list"
+        context {}]))
 
-(defn with-full-url [{:keys [uri host] :as request-map} context]
-  (let [resolved-uri (apply format uri (:path-params context))]
-    (assoc request-map :url (str host resolved-uri))))
-(defn request!
-  [host api-token timeout endpoint context]
-  (let [base-request-raw (merge (base-request host api-token timeout))
-        endpoint-request (get endpoints/request endpoint {})
-        request-map-raw  (merge base-request-raw endpoint-request context)
-        request-map      (with-full-url request-map-raw context)
-        response         (or (:response-fn request-map) identity)]
-    (-> request-map
-        sni-client/req!
-        :body
-        response)))
+
+(defn request2! [token timeout host endpoint context]
+  (-> {:url (str host endpoint)
+       :method :get
+       :timeout timeout
+       :accept :json
+       :headers {"Authorization" (str "Bearer " token)}}
+      http/request
+      deref
+      :body
+      (json/read-str :key-fn keyword)))
+
+(comment
+  (request2! "ad" 10000 "http://dummy.restapiexample.com" "/api/v1/employees" {}))
+
+;; (defn with-full-url [{:keys [uri host] :as request-map} context]
+;;   (let [resolved-uri (apply format uri (:path-params context))]
+;;     (assoc request-map :url (str host resolved-uri))))
+;; (defn request!
+;;   [host api-token timeout endpoint context]
+;;   (let [base-request-raw (merge (base-request host api-token timeout))
+;;         endpoint-request (get endpoints/request endpoint {})
+;;         request-map-raw  (merge base-request-raw endpoint-request context)
+;;         request-map      (with-full-url request-map-raw context)
+;;         response         (or (:response-fn request-map) identity)]
+;;     (-> request-map
+;;         sni-client/req!
+;;         :body
+;;         response)))
