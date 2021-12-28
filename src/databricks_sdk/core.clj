aebd4eb55d824b4a72c5cefceab10bb101c1f9e0
Marina Cavalari <marina.cavalari.mc@gmail.com>
2021-12-28 12:22:16 -0300

    refactor docstring

--- src/databricks_sdk/core.clj
+++ src/databricks_sdk/core.clj
@@ -1,33 +1,41 @@
 (ns databricks-sdk.core
   (:require [databricks-sdk.impl.databricks :as impl]))
 
-(defn create-cluster! 
-  "Create a new cluster in a determinated Databricks account.
-   Options must have at least [token timeout host context]
-     :token your databricks personal token to use the API
-     :timeout to connect and request
-     :host your account host, it should be something like: http://abc.cloud.databricks.com
-     :context is your data to the request, in this case use a cluster information, as below:
-   {:cluster_name 'my-cluster'
-    :spark_version '7.3.x-scala2.12' 
-    :node_type_id  'i3.xlarge'                               
-    :spark_conf {:spark.speculation true}
-    :aws_attributes {:availability 'SPOT'
-                     :zone_id  'us-west-2a'} 
-    :num_workers 25}
-   And returns a new cluster-id "
+(defn create-cluster!
+  "Create a new cluster within a Databricks Account
+   **Input**
+   Must have at least [token timeout host context]
+   - `:token` your databricks token to use the API
+   - `:timeout` the time limit in milliseconds to wait for the response.
+   - `:host` your account host, e.g. `http://abc.cloud.databricks.com`
+   - `:context` is your data to the request, in this case use a cluster information, as below:
+  ```clojure
+     {:cluster_name 'my-cluster'
+      :spark_version '7.3.x-scala2.12'
+      :node_type_id 'i3.xlarge'
+      :spark_conf {:spark.speculation true}
+      :aws_attributes {:availability 'SPOT'
+                       :zone_id  'us-west-2a'}
+      :num_workers 25}
+ ```
+  **Output**
+   Returns the new created `cluster-id`."
   [options]
   (-> options
       (assoc :endpoint :clusters/list)
       impl/request!))
 
-(defn list-clusters 
+(defn list-clusters
   "List all clusters from a databrick account.
-   Options must have at least [token timeout host context]
-     :token your databricks personal token to use the API
-     :timeout to connect and request
-     :host your account host, it should be something like: http://abc.cloud.databricks.com
-   And its returns is the clusters list itself"
+   **Input**
+    Must have at least [token timeout host context]
+    - `:token` your databricks token to use the API
+    - `:timeout` the time limit in milliseconds to wait for the response.
+    - `:host` your account host, e.g. `http://abc.cloud.databricks.com`
+    - `:context` is your data to the request, in this case use empty {}
+   
+   **Output**
+   And its returns is the `clusters` list itself"
   [options]
   (-> options
       (assoc :endpoint :clusters/list)
