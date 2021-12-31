e5fc544f224e64915cf977790e64c1a9b333c107
Marina Cavalari <marina.cavalari.mc@gmail.com>
2021-12-30 22:26:38 -0300

    refactor function to handle error

--- src/databricks_sdk/impl/databricks.clj
+++ src/databricks_sdk/impl/databricks.clj
@@ -3,15 +3,6 @@
    [databricks-sdk.impl.endpoints :as endpoints]
    [org.httpkit.client :as http]))
 
-(def response-status
-  {:400 "Unable to resolve, reason: Bad request"
-   :200 "Success"
-   :500 "Unable to resolve, reason: Internal Error"
-   :201 "Created"
-   :401 "Unable to resolve, reason: Unauthorized"
-   :406 "Unable to resolve, reason: Method not allowed"
-   :429 "Unable to resolve, reason: Too many requests"})
-
 (defn request-raw [{:keys [token timeout host endpoint context]}]
   (-> {:url (str host (-> endpoints/request endpoint :uri))
        :method (-> endpoints/request endpoint :method)
@@ -25,10 +16,11 @@
 (defn request! [options]
   (let [request (-> options
                     request-raw
-                    (select-keys [:body :status]))
-        status (get response-status (keyword (str (:status request))))]
-    (if (= (:status request) (or 200 201))
-      request
+                    (select-keys [:body :status :error]))
+        error   (:error request)
+        status  (:status request)]
+    (if error
+      (str error)
       {:status status})))
 
 
