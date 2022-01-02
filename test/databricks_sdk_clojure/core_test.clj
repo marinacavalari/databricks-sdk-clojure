ed236f795393509f0b5e3ae9b73b8554ef82fce0
Marina Cavalari <marina.cavalari.mc@gmail.com>
2022-01-01 22:46:13 -0300

    add first test

--- test/databricks_sdk_clojure/core_test.clj
+++ test/databricks_sdk_clojure/core_test.clj
@@ -1,8 +1,19 @@
 (ns databricks-sdk-clojure.core-test
   (:require
    [clojure.test :refer :all]
-   [databricks-sdk-clojure.core :refer :all]))
+   [databricks-sdk.core :as sdk]
+   [org.httpkit.client :as http]
+   [clojure.data.json :as json]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defn success-cluster-list-response [test-function]
+  (future
+    {:body "{\"clusters\":[{\"cluster_id\":\"123\"}]}"
+     :status 200}))
+
+(deftest list-clusters-list
+  (testing "success"
+    (with-redefs [http/request success-cluster-list-response]
+      (is (= {:clusters [{:cluster_id "123"}]}(sdk/list-clusters {:token "bcfGe428JL09"
+                                                                  :timeout 30000
+                                                                  :host "https://example-account.cloud.databricks.com"
+                                                                  :context {}}))))))
