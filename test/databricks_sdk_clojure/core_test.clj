ce997645f8962a5ce088db7cbeae2a5fba3adb6e
Marina Cavalari <marina.cavalari.mc@gmail.com>
2022-01-04 20:39:19 -0300

    add test to edit

--- test/databricks_sdk_clojure/core_test.clj
+++ test/databricks_sdk_clojure/core_test.clj
@@ -4,19 +4,19 @@
    [databricks-sdk.core :as sdk]
    [org.httpkit.client :as http]))
 
-(defn success-cluster-list-response [test-function]
+(defn success-cluster-response [test-function]
   (future
     {:body "{\"clusters\":[{\"cluster_id\":\"123\"}]}"
      :status 200}))
 
-(defn failed-cluster-list-response [test-function]
+(defn failed-response [test-function]
   (future
     {:body "Too many requests"
      :status 429}))
 
 (deftest list-clusters-list
-  (testing "success, returning the request's body"
-    (with-redefs [http/request success-cluster-list-response]
+  (testing "success, returning the request's body, in this case, the clusters list"
+    (with-redefs [http/request success-cluster-response]
       (is (= {:result {:clusters [{:cluster_id "123"}]}}
              (sdk/list-clusters {:token "bcfGe428JL09"
                                  :timeout 30000
@@ -32,10 +32,34 @@
                                :context {}}))))
   
   (testing "failed, known status error"
-    (with-redefs [http/request failed-cluster-list-response]                      
+    (with-redefs [http/request failed-response]                      
      (is (= {:error {:code 429
                      :message  "Too many requests"}}
             (sdk/list-clusters {:token "bcfGe428JL09"
                                 :timeout 30000
                                 :host "https://example-account.cloud.databricks.com"
                                 :context {}}))))))
+
+(deftest edit-cluster-test
+  (testing "success, returning the request's body, in this case, the cluster id"
+    (with-redefs [http/request success-cluster-response]
+      (is (= {:result {:clusters [{:cluster_id "123"}]}}
+             (sdk/edit-cluster! {:token "bcfGe428JL09"
+                                 :timeout 30000
+                                 :host "https://example-account.cloud.databricks.com"
+                                 :context {}})))))
+  (testing "failed, uknown status error"
+    (is (= {:error {:code 1
+                    :message "example-account.cloud.databricks.com: nodename nor servname provided, or not known"}}
+           (sdk/edit-cluster! {:token "bcfGe428JL09"
+                               :timeout 30000
+                               :host "https://example-account.cloud.databricks.com"
+                               :context {}}))))
+  (testing "failed, known status error"
+    (with-redefs [http/request failed-response]
+      (is (= {:error {:code 429
+                      :message  "Too many requests"}}
+             (sdk/edit-cluster! {:token "bcfGe428JL09"
+                                 :timeout 30000
+                                 :host "https://example-account.cloud.databricks.com"
+                                 :context {}}))))))
