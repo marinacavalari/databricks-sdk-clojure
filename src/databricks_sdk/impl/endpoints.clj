d9d2986660974d6fa2eec5917862180e7ae503d2
Marina Cavalari <marina.cavalari.mc@gmail.com>
2022-01-04 20:59:18 -0300

    change endpoints

--- src/databricks_sdk/impl/endpoints.clj
+++ src/databricks_sdk/impl/endpoints.clj
@@ -12,21 +12,7 @@
    :clusters/start             {:method :post
                                 :uri    "/api/2.0/clusters/start"}
    :clusters/terminate         {:method :post
-                                :uri    "/api/2.0/clusters/delete"}
-   :libraries/install          {:method :post
-                                :uri    "/api/2.0/libraries/install"}
-   :libraries/status           {:method :get
-                                :uri    "/api/2.0/libraries/cluster-status?cluster_id={{cluster-id}}"}
-   :clusters/permissions       {:method :patch
-                                :uri    "/api/2.0/permissions/clusters/{{cluster-id}}"}
-   :scim/users                 {:method :get
-                                :uri    "/api/2.0/preview/scim/v2/Users"}
-   :scim/get-user-id           {:method :get
-                                :uri    "/api/2.0/preview/scim/v2/Users?filter=userName+eq+{{email}}"}
-   :scim/remove-role-from-user {:method :patch
-                                :uri    "/api/2.0/preview/scim/v2/Users/{{databricks-user-id}}"}
-   :workspace/list             {:method :get
-                                :uri    "/api/2.0/workspace/list"}})
+                                :uri    "/api/2.0/clusters/delete"}})
 
 
 
