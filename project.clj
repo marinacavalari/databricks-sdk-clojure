ac46633def1a5d4dcd8bdeaaa324ec8e89a7a4b4
Marina Cavalari <marina.cavalari.mc@gmail.com>
2022-01-04 21:23:30 -0300

    fix lib

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.marinacavalari/databricks-sdk "0.1.0-SNAPSHOT"
+(defproject org.clojars.marinacavalari/databricks-sdk "0.1.1"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
