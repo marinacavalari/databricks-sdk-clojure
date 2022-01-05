2f42a15e7bef03e920b5a1427002dae6ab68f276
Marina Cavalari <marina.cavalari.mc@gmail.com>
2022-01-04 21:07:47 -0300

    add t4erminate function

--- src/databricks_sdk/core.clj
+++ src/databricks_sdk/core.clj
@@ -11,16 +11,19 @@
    - `:host` your account host, e.g. `http://abc.cloud.databricks.com`
    - `:context` is your data to the request, in this case use a cluster information, as below:
   ```clojure
-     {:cluster_name 'my-cluster'
-      :spark_version '7.3.x-scala2.12'
-      :node_type_id 'i3.xlarge'
+     {:cluster_name \"my-cluster\"
+      :spark_version \"7.3.x-scala2.12\"
+      :node_type_id \"i3.xlarge\"
       :spark_conf {:spark.speculation true}
-      :aws_attributes {:availability 'SPOT'
-                       :zone_id  'us-west-2a'}
+      :aws_attributes {:availability \"SPOT\"
+                       :zone_id  \"us-west-2a\"}
       :num_workers 25}
   ```
   **Output**
-   Returns result and the the new created `cluster-id`."
+   
+   ```clojure 
+      {:result {:cluster_id \"123\"}}
+   ``` "
   [options]
   (-> options
       (assoc :endpoint :clusters/list)
@@ -36,7 +39,10 @@
     - `:context` is your data to the request, in this case use empty {}
    
    **Output**
-   And its returns is the result and `clusters` list itself"
+   
+   ```clojure 
+      {:result {:clusters [{:cluster_id \"123\"}]}}
+   ``` "
   [options]
   (-> options
       (assoc :endpoint :clusters/list)
@@ -51,14 +57,16 @@
    - `:host` your account host, e.g. `http://abc.cloud.databricks.com`
    - `:context` is your data to the request, in this case use a cluster information, as below:
   ```clojure
-     {:cluster_id '1x2e34r5t'
-      :spark_version '7.3.x-scala2.12'
-      :node_type_id 'i3.xlarge'
+     {:cluster_id \"1x2e34r5t\"
+      :spark_version \"7.3.x-scala2.12\"
+      :node_type_id \"i3.xlarge\"
       :num_workers 10}
  ```
   **Output**
    
-   {:result {:body \"cluster_id\"}}"
+  ```clojure 
+    {:result {:cluster_id \"123\"}}
+  ``` "
   [options]
   (-> options
       (assoc :endpoint :clusters/edit)
@@ -73,11 +81,35 @@
    - `:host` your account host, e.g. `http://abc.cloud.databricks.com`
    - `:context` is your data to the request, in this case use a cluster id information, as below:
   ```clojure
-     {:cluster_id '1x2e34r5t'}
+     {:cluster_id \"1x2e34r5t\"}
  ```
-  **Output**
-   {:result {:body \"<cluster_id>\"}}"
+   **Output**
+   
+  ```clojure 
+     {:result {:cluster_id \"123\"}}
+  ``` "
   [options]
   (-> options
       (assoc :endpoint :clusters/start)
+      impl/request!))
+
+(defn terminate-cluster!
+  "Terminate a specific cluster within a Databricks Account
+   **Input**
+   Must have at least [token timeout host context]
+   - `:token` your databricks token to use the API
+   - `:timeout` the time limit in milliseconds to wait for the response.
+   - `:host` your account host, e.g. `http://abc.cloud.databricks.com`
+   - `:context` is your data to the request, in this case use a cluster id information, as below:
+  ```clojure
+     {:cluster_id \"1x2e34r5t\"}
+ ```
+   **Output**
+   
+  ```clojure 
+    {:result {:cluster_id \"123\"}}
+  ``` "
+  [options]
+  (-> options
+      (assoc :endpoint :clusters/terminate)
       impl/request!))
